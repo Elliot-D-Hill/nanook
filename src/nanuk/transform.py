cab452727ea32825ad9eb865b511610698fef031
Elliot Hill <elliot.douglas.hill@gmail.com>
2024-11-25 10:11:04 -0500

    Fix edge case for split assignment

--- src/nanuk/transform.py
+++ src/nanuk/transform.py
@@ -2,6 +2,7 @@ import warnings
 
 import polars as pl
 import polars.selectors as cs
+from polars._typing import IntoExpr
 
 from nanuk.typing import Impute, Standardize
 
@@ -10,7 +11,7 @@ def check_splits(splits: dict[str, float]) -> dict[str, float]:
     split_sum = sum(splits.values())
     splits = {split: size / split_sum for split, size in splits.items()}
     if split_sum != 1.0:
-        warnings.warn(f"Split proportions were normalized to sum to 1.0: {splits}")
+        warnings.warn(f"Split proportions were normalized to sum to 1.0: {str(splits)}")
     return splits
 
 
@@ -21,15 +22,13 @@ def assign_splits(
     lower = 0.0
     expr = pl.when(False).then(None)
     index = pl.col(group).rank(method="dense").sub(1)
-    for split, size in splits.items():
+    items = list(splits.items())
+    for split, size in items[:-1]:
         upper = lower + size * index.max()
         expr = expr.when(index.is_between(lower, upper)).then(pl.lit(split))
         lower = upper
-    return expr.alias(name)
-
-
-def if_over(expr: pl.Expr, over: str | list[str] | pl.Expr | None) -> pl.Expr:
-    return pl.when(over is not None).then(expr.over(over)).otherwise(expr)
+    last_split = items[-1][0]
+    return expr.otherwise(pl.lit(last_split)).alias(name)
 
 
 def safe_divide(numerator: pl.Expr, denominator: pl.Expr) -> pl.Expr:
@@ -74,8 +73,12 @@ def impute(expr: pl.Expr, method: str, train: pl.Expr | None = None) -> pl.Expr:
     return expr.fill_null(train)
 
 
+def if_over(expr: pl.Expr, over: IntoExpr) -> pl.Expr:
+    return pl.when(over is not None).then(expr.over(over)).otherwise(expr)
+
+
 def pipeline[T: (pl.DataFrame, pl.LazyFrame)](
-    df: T, transforms: list[pl.Expr], over: str | None = None
+    df: T, transforms: list[pl.Expr], over: IntoExpr = None
 ) -> T:
     for transform in transforms:
         transform = if_over(expr=transform, over=over)
