724ebb612b177d44d12432393183f0466a57d960
Elliot Hill <elliot.douglas.hill@gmail.com>
2024-11-25 10:10:27 -0500

    Refactor conditional selection

--- src/nanuk/preprocess.py
+++ src/nanuk/preprocess.py
@@ -1,20 +1,28 @@
 import polars as pl
+import polars.selectors as cs
 
 from nanuk.frame import collect_if_lazy
 
 
+def select_by_condition[T: (pl.DataFrame, pl.LazyFrame)](
+    df: T, condition: pl.Expr
+) -> T:
+    condition_df = collect_if_lazy(df.select(condition).unpivot())
+    columns_to_keep = condition_df.filter(pl.col("value"))["variable"]
+    return df.select(cs.by_name(columns_to_keep))
+
+
 def drop_null_columns[T: (pl.DataFrame, pl.LazyFrame)](df: T, cutoff: float) -> T:
-    n_rows = df.select(pl.len())
-    n_rows = collect_if_lazy(n_rows).item()
-    column_is_null = pl.all().null_count().truediv(n_rows).lt(cutoff)
-    columns = collect_if_lazy(df.select(column_is_null)).to_dicts()[0]
-    columns_to_keep = [col for col, is_null in columns.items() if is_null]
-    return df.select(columns_to_keep)
+    is_null = pl.all().null_count().truediv(pl.len()).lt(cutoff)
+    return select_by_condition(df=df, condition=is_null)
 
 
 def filter_null_rows[T: (pl.DataFrame, pl.LazyFrame)](df: T, columns: pl.Expr) -> T:
     return df.filter(~pl.all_horizontal(columns.is_null()))
 
 
-def drop_zero_variance[T: (pl.DataFrame, pl.LazyFrame)](df: T, cutoff: float) -> T:
-    return df.select(pl.all().var().gt(cutoff))
+def drop_zero_variance[T: (pl.DataFrame, pl.LazyFrame)](
+    df: T, cutoff: float = 1e-8
+) -> T:
+    has_zero_variance = pl.all().var().gt(cutoff)
+    return select_by_condition(df=df, condition=has_zero_variance)
